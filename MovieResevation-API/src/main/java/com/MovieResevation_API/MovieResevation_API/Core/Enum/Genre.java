42f918e77c5060879998c6a80b9bb289edb0290b
Matheus Schingiry <matheussschingiry@gmail.com>
2026-01-20 23:41:56 -0300

    feat(domain): create Movie entity

---
+++ MovieResevation-API/src/main/java/com/MovieResevation_API/MovieResevation_API/Core/Enum/Genre.java
@@ -0,0 +1,14 @@
+package com.MovieResevation_API.MovieResevation_API.Core.Enum;
+
+public enum Genre {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCI_FI,
+    ROMANCE,
+    THRILLER,
+    FANTASY,
+    ANIMATION,
+    DOCUMENTARY
+}
