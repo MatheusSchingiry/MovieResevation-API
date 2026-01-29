becb5bc783780be95e8b04631410b46782fefd5c
Matheus Schingiry <matheussschingiry@gmail.com>
2026-01-28 23:22:34 -0300

    refactor: remove movie domain and persistence files

--- MovieResevation-API/src/main/java/com/MovieResevation_API/MovieResevation_API/infrastructure/Persistence/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.MovieResevation_API.MovieResevation_API.infrastructure.Persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
-}
